5380fcf3e1734a10bb8e8b5f3b67b95754569f0c
lgants <lgants@gmail.com>
2016-07-11 17:44:00 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,88 @@
+=begin
+INPUT
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
+
+OUTPUT
+{
+  :ruby => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :javascript => {
+    :type => "interpreted",
+    :style => [:oo, :functional]
+  },
+  :python => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :java => {
+    :type => "compiled",
+    :style => [:oo]
+  },
+  :clojure => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :erlang => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :scala => {
+    :type => "compiled",
+    :style => [:functional]
+  }
+}
+
+=end 
+  
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |paradigm, language|
+    language.each do |language, type|
+      type.each do |type, value|
+        if language == :javascript
+          new_hash[language] = {
+            :type => value,
+            :style => [:oo, :functional]
+          }
+        else 
+          new_hash[language] = {
+            :type => value,
+            :style => [paradigm]
+          }
+        end 
+      end
+    end  
+  end 
+  new_hash
 end
